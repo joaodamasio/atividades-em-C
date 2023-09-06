52856220643292c7fdef511239df47b2cb8336a8
Messias <125613915+joaodamasio@users.noreply.github.com>
2023-09-06 10:58:18 -0300

    atividade8

---
+++ qtdBytes.cpp
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    printf("Tamanho de int: %zu bytes\n", sizeof(int));
+    printf("Tamanho de char: %zu bytes\n", sizeof(char));
+    printf("Tmanho de float: %zu bytes\n", sizeof(float));
+    printf("Tamanho de double: %zu bytes\n", sizeof(double));
+
+    return 0;
+}
